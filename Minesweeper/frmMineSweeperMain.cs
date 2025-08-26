377480d5cdd261d94847546f486b3913b46555ae
Bojan Kozina <bojan.kozina@edu.fit.ba>
2025-08-26 17:36:30 +0200

    Merge everything

--- Minesweeper/frmMineSweeperMain.cs
+++ Minesweeper/frmMineSweeperMain.cs
@@ -14,11 +14,13 @@ namespace Minesweeper
 
         private void btnExit_Click(object sender, EventArgs e)
         {
+            //MakeHover
             Application.Exit();
         }
 
         private void btnMinimize_Click(object sender, EventArgs e)
         {
+            //MakeIconsChange
             this.WindowState = FormWindowState.Minimized;
         }
 
